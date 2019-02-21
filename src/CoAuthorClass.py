178be9421fa2b695f1ec6ab877bdbf95254e8abb
trnkv <ttrnkv75@yandex.ru>
2019-02-21 13:12:45 +0300

    Delete CoAuthorClass.py

--- src/CoAuthorClass.py
+++
@@ -1,4 +0,0 @@
-from PersonClass import Person
-class CoAuthor(Person):
-    def __init__(self, firstName, familyName, email, affiliation):
-        super().__init__(firstName, familyName, email, affiliation)
