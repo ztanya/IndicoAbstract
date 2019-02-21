d8df9aa16ed192b3486e387ff1d6f07bef7fc373
trnkv <ttrnkv75@yandex.ru>
2019-02-21 13:12:55 +0300

    Delete PrimaryAuthorClass.py

--- src/PrimaryAuthorClass.py
+++
@@ -1,4 +0,0 @@
-from PersonClass import Person
-class PrimaryAuthor(Person):
-    def __init__(self, firstName, familyName, email, affiliation):
-        super().__init__(firstName, familyName, email, affiliation)
